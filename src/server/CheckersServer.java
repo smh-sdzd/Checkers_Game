1344192e5b449ef0dac6817c86801dbc3c8e1b70
SMH. SdZd <smh.sdzd@gmail.com>
2026-07-06 01:06:23 +0330

    Add server socket setup.

---
+++ src/server/CheckersServer.java
@@ -0,0 +1,12 @@
+package server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CheckersServer {
+    public static void main(String[] args) {
+
+        System.out.println("Server started");
+
+    }
+}
