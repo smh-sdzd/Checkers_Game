2b5102be1493f25693c5b5fd1018fb576151d9bc
SMH. SdZd <smh.sdzd@gmail.com>
2026-07-06 14:51:21 +0330

    Initialize CheckersServer

--- src/server/CheckersServer.java
+++ src/server/CheckersServer.java
@@ -7,10 +7,16 @@ public class CheckersServer {
     public static void main(String[] args) {
         try {
         ServerSocket server = new ServerSocket(5000);
-        System.out.println("Server is waiting for a player...");
+        
+        System.out.println("Server started.");
+        System.out.println("Waiting for a player to connect...");
+        
+        Socket client = server.accept();
+        System.out.println("A player connected!");
+
         }
         catch (Exception e) {
-        e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
