f8ea8f7da76348999f8c880940d80de1084ae111
Alireza <alireza.ark01@gmail.com>
2026-07-21 23:37:16 +0330

    fix bug

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -26,22 +26,29 @@ public class Piece implements Serializable {
 
     public Color getColor() { return color; }
     public void setColor(Color color) { this.color = color; }
+
     public Type getType() { return type; }
     public void setType(Type type) { this.type = type; }
+
     public int getRow() { return row; }
     public void setRow(int row) { this.row = row; }
+
     public int getCol() { return col; }
     public void setCol(int col) { this.col = col; }
+
+
     public void makeKing() { this.type = Type.KING; }
+
     public boolean isRed() { return this.color == Color.RED; }
     public boolean isBlack() { return this.color == Color.BLACK; }
     public boolean isKing() { return this.type == Type.KING; }
+
+
 
     /**
      * Returns forward direction based on color.
      * BLACK moves upward (-1), RED moves downward (+1).
      */
-
     public int getForwardDirection() {
         return (this.color == Color.BLACK) ? -1 : 1;
     }
